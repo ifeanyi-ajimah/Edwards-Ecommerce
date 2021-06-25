7468f9245161baa70277fe65c2e3712ecdc82ea3
ifeanyi-ajimah <ajimahifeanyi@gmail.com>
2021-06-25 12:43:23 +0100

    meta tags

--- resources/views/adminlayouts/header.blade.php
+++ resources/views/adminlayouts/header.blade.php
@@ -2,7 +2,12 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Dashboard  </title>
+    <title> REISS EDWARDS SHOP  </title>
+    
+    <meta name="keyword" content="Shop, Fruit shop, good , clothes ">
+	<meta name="author" content="Reiss Edwards ">
+  <meta name="description" content="the most awesome shop you can ever imagine">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,400i,600,700,800,900" rel="stylesheet" />
     <link href="{{ url('gull/dist-assets/css/themes/lite-purple.min.css')}}" rel="stylesheet" />
